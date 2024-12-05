6cac686245af4425e69c18451febdb389d10dbd9
QP <quaepoena@example.com>
2024-12-05 11:21:27 +0100

    endra setup.sh på lik måte som med humit-språkidentifikator

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-cat models/sentence_segmentation/x* > models/sentence_segmentation/pytorch_model.bin
-cat models/classification/x* > models/classification/pytorch_model.bin
-cat models/tokenization/x* > models/tokenization/pytorch_model.bin
-rm models/*/x*
+
+ls models/*/x* &>/dev/null
+if [[ "$?" -eq 0 ]]; then
+    cat models/sentence_segmentation/x* > models/sentence_segmentation/pytorch_model.bin
+    cat models/classification/x* > models/classification/pytorch_model.bin
+    cat models/tokenization/x* > models/tokenization/pytorch_model.bin
+    rm models/*/x*
+fi
+
 pip3 install -r requirements.txt
