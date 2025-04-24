7c3a96fba9e18b2c7838486bbf284bc40df1b833
Ingerid Dale <ingerid.dale@nb.no>
2025-04-24 10:23:43 +0200

    specify shell interpreter

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 ls models/*/x* &>/dev/null
+
 if [[ "$?" -eq 0 ]]; then
     cat models/sentence_segmentation/x* > models/sentence_segmentation/pytorch_model.bin
     cat models/classification/x* > models/classification/pytorch_model.bin
     cat models/tokenization/x* > models/tokenization/pytorch_model.bin
-    rm models/*/x*
+    #rm models/*/x*
 fi
 
 pip3 install -r requirements.txt
